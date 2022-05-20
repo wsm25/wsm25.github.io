f3bd7130f4d565dd441b40def589b23a684a747d
wsm <wsm_25@qq.com>
2022-05-20 12:29:09 +0800

    TikTacToe in Vue3

--- TikTacToe/main.js
+++ TikTacToe/main.js
@@ -1,100 +1,110 @@
-function wslice(list,slist){
-    var out=new Array();
-    let len=list.length;
-    for(let i=0;i<len;++i){
-        if (slist.includes(i)){
-            out.push(list[i]);}}
-    return out;
-}
-
-const judgeArray=[
-    [1,2,3],[4,5,6],[7,8,9],
-    [1,4,7],[2,5,8],[3,6,9],
-    [1,5,9],[3,5,7]
-]
-
-function judge(list){
-    for(arr of judgeArray){
-        if (wslice(list,arr).join('').length==arr.length){
-            return true;
-        }
+function wslice(list, slist) {
+  var out = new Array();
+  let len = list.length;
+  for (let i = 0; i < len; ++i) {
+    if (slist.includes(i)) {
+      out.push(list[i]);
     }
-    return false;
+  }
+  return out;
 }
 
-var xostate='X'
+const judgeArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
-const game={
-    data(){
-        return {
-            getBoard:[
-            [1,2,3],
-            [4,5,6],
-            [7,8,9]
-            ],
-            winned:'',
-            length:10,
-            xList:new Array(10).fill(''),
-            oList:new Array(10).fill(''),
-        }
-    },
-    methods:{
-        pushXo(id){
-            if (this.xoList[id]==='' && this.winned===''){
-                if(xostate==='X'){
-                    this.xList[id]='X';
-                    if(judge(this.xList)){
-                        this.winned='X';
-                    }
-                }else{
-                    this.oList[id]='O';
-                    if(judge(this.oList)){
-                        this.winned='O';
-                    }
-                }
-                xostate=(xostate==='X'?'O':'X');
-            }
-        },
-        clear(){
-            this.xList = new Array(10).fill('');
-            this.oList = new Array(10).fill('');
-            this.winned = ''
-        }
-    },
-    computed:{
-        xoList(){
-            tmp=new Array(this.length).fill('');
-            for (let i=1;i<10;++i){
-                tmp[i]=
-                  (this.xList[i]==='')?
-                  this.oList[i]:this.xList[i];
-            }
-            return tmp;
-        },
-        notice(){
-            if (this.winned!=''){
-                return this.winned+' Wins!!!'
-            }
-            return ''
-        }
+function judge(list) {
+  for (arr of judgeArray) {
+    if (wslice(list, arr).join("").length == arr.length) {
+      return true;
     }
+  }
+  return false;
 }
 
-const Square={
-    props: ['clear','xo'],
-    emits: ['pushXo'],
-    computed:{
-        visible(){return this.xo===''?false:true;},
+var xostate = "X";
+
+const game = {
+  data() {
+    return {
+      getBoard: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ],
+      winned: "",
+      len: 10,
+      xList: new Array(10).fill(""),
+      oList: new Array(10).fill(""),
+    };
+  },
+  methods: {
+    pushXo(id) {
+      if (this.xoList[id] === "" && this.winned === "") {
+        if (xostate === "X") {
+          this.xList[id] = "X";
+          if (judge(this.xList)) {
+            this.winned = "X";
+          }
+        } else {
+          this.oList[id] = "O";
+          if (judge(this.oList)) {
+            this.winned = "O";
+          }
+        }
+        xostate = xostate === "X" ? "O" : "X";
+      }
     },
-    methods:{},
-    template:`
+    clear() {
+      this.xList = new Array(10).fill("");
+      this.oList = new Array(10).fill("");
+      this.winned = "";
+    },
+  },
+  computed: {
+    xoList() {
+      tmp = new Array(this.len).fill("");
+      for (let i = 1; i < 10; ++i) {
+        tmp[i] = this.xList[i] === "" ? this.oList[i] : this.xList[i];
+      }
+      return tmp;
+    },
+    notice() {
+      if (this.winned != "") {
+        return this.winned + " Wins!!!";
+      }
+      if (this.xoList.join("").length === 9) {
+        return "You've got a draw";
+      }
+      return "";
+    },
+  },
+};
+
+const Square = {
+  props: ["clear", "xo"],
+  emits: ["pushXo"],
+  computed: {
+    visible() {
+      return this.xo === "" ? false : true;
+    },
+  },
+  methods: {},
+  template: `
     <button class="square" @click="this.$emit('pushXo')
     ">
         <div v-if="visible">{{this.xo}}</div>
     </button>
-    `
-}
+    `,
+};
 
-const app=Vue.createApp(game)
-app.component('square',Square)
-app.mount('#game')
+const app = Vue.createApp(game);
+app.component("square", Square);
+app.mount("#game");
